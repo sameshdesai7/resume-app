84ee4dbe480d8af765450b36bfe642891f91ddc5
sameshdesai7 <srd00013@mix.wvu.edu>
2024-08-28 18:27:58 -0400

    Removed example page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+/**
+ * App is the main component of the application.
+ * It renders a hamburger-shaped icon, which is a placeholder for the actual
+ * application content.
+ */
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Testing
     </div>
   );
 }
