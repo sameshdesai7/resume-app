41ff027e5943de1d7d390af8b21f7671b69a71b9
desai <srd00013@mix.wvu.edu>
2024-08-29 13:37:16 -0400

    moved autotyper out of hompage and into its own component

--- src/custom-components/autotyper.tsx
+++ src/custom-components/autotyper.tsx
@@ -3,21 +3,38 @@ import { TypeAnimation } from 'react-type-animation';
 export default function AutoTyper() {
   return (
     <TypeAnimation
-      sequence={[
-        // Same substring at the start will only be typed out once, initially
-        'We produce food for Mice',
-        1000, // wait 1s before replacing "Mice" with "Hamsters"
-        'We produce food for Hamsters',
-        1000,
-        'We produce food for Guinea Pigs',
-        1000,
-        'We produce food for Chinchillas',
-        1000
-      ]}
-      wrapper="span"
-      speed={50}
-      style={{ fontSize: '2em', display: 'inline-block' }}
-      repeat={Infinity}
-    />
+                                preRenderFirstString={true}
+                                sequence={[
+                                    // Same substring at the start will only be typed out once, initially
+                                    'I am proficient in TypeScript',
+                                    1000, 
+                                    'I am proficient in React',
+                                    1000,
+                                    'I am proficient in Java',
+                                    1000,
+                                    'I am proficient in Python',
+                                    1000,
+                                    'I am proficient in C#',
+                                    1000,
+                                    'I am proficient in HTML',
+                                    1000,
+                                    'I am proficient in C',
+                                    1000,
+                                    'I am proficient in .NET',
+                                    1000,
+                                    'I am proficient in Agile',
+                                    1000,
+                                    'I am proficient in GitHub',
+                                    1000,
+                                    'I am proficient in Jira',
+                                    1000,
+                                    'I am proficient in Linux',
+                                    1000,
+                                ]}
+                                wrapper="span"
+                                speed={50}
+                                style={{ fontSize: '1em', display: 'inline-block' }}
+                                repeat={Infinity}
+                            />
   );
 };
--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -27,41 +27,7 @@ export default function Homepage() {
                                 I'm Samesh Desai
                             </Typography>
 
-                            {/* This should be moved to its own component */}
-                            <TypeAnimation
-                                preRenderFirstString={true}
-                                sequence={[
-                                    // Same substring at the start will only be typed out once, initially
-                                    'I am proficient in TypeScript',
-                                    1000, 
-                                    'I am proficient in React',
-                                    1000,
-                                    'I am proficient in Java',
-                                    1000,
-                                    'I am proficient in Python',
-                                    1000,
-                                    'I am proficient in C#',
-                                    1000,
-                                    'I am proficient in HTML',
-                                    1000,
-                                    'I am proficient in C',
-                                    1000,
-                                    'I am proficient in .NET',
-                                    1000,
-                                    'I am proficient in Agile',
-                                    1000,
-                                    'I am proficient in GitHub',
-                                    1000,
-                                    'I am proficient in Jira',
-                                    1000,
-                                    'I am proficient in Linux',
-                                    1000,
-                                ]}
-                                wrapper="span"
-                                speed={50}
-                                style={{ fontSize: '1em', display: 'inline-block' }}
-                                repeat={Infinity}
-                            />
+                            <AutoTyper />
 
                             <Button variant="contained" sx={{ maxWidth: "150px" }}>
                                 View More!
