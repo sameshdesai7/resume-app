30e52ec395d1b15bdc1585f435da0b96a18fc126
desai <srd00013@mix.wvu.edu>
2024-08-29 11:15:50 -0400

    Created intial homepage layout

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,12 +1,30 @@
-import { Box, Card, Typography } from "@mui/material";
+import { Box, Card, Stack, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid2";
 import BiographyCard from '../custom-components/biography-card';
 
 export default function Homepage() {
     return (
-        <Box sx = {{margin : "2%"}}>
-            <Box>
-                <BiographyCard></BiographyCard>
-            </Box>
-        </Box>
+
+        // Page container 
+        <Stack sx={{ margin: "2%" }}>
+            {/* Grid for biography card and other menu */}
+            <Grid container spacing={2}>
+                <Grid size={6}>
+                    <Box sx={{ backgroundColor: "#000000" }}>
+                        {/*Left Side Content Here  */}
+                    </Box>
+
+                </Grid>
+                <Grid size={6}>
+
+                    <Box sx={{ backgroundColor: "#000000" }}>
+                        {/* Right Side Content here  */}
+                    </Box>
+                </Grid>
+            </Grid>
+
+            {/* Items below will go here  */}
+        </Stack>
+
     )
 }
