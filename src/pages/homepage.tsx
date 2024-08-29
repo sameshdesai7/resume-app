92b2f3c3a636ca4eab516d4468bc5f9731f6eb47
desai <srd00013@mix.wvu.edu>
2024-08-29 12:22:10 -0400

    Added more autotyper contents

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -26,27 +26,40 @@ export default function Homepage() {
                             <Typography variant="h2">
                                 I'm Samesh Desai
                             </Typography>
-                            <Typography variant="h6">
-                                placeholder text
-                            </Typography>
 
                             {/* This should be moved to its own component */}
                             <TypeAnimation
                                 preRenderFirstString={true}
                                 sequence={[
                                     // Same substring at the start will only be typed out once, initially
-                                    'We produce food for Mice',
+                                    'I am proficient in TypeScript',
                                     1000, // wait 1s before replacing "Mice" with "Hamsters"
-                                    'We produce food for Hamsters',
+                                    'I am proficient in React',
                                     1000,
-                                    'We produce food for Guinea Pigs',
+                                    'I am proficient in Java',
                                     1000,
-                                    'We produce food for Chinchillas',
-                                    1000
+                                    'I am proficient in Python',
+                                    1000,
+                                    'I am proficient in C#',
+                                    1000,
+                                    'I am proficient in HTML',
+                                    1000,
+                                    'I am proficient in C',
+                                    1000,
+                                    'I am proficient in .NET',
+                                    1000,
+                                    'I am proficient in Agile',
+                                    1000,
+                                    'I am proficient in GitHub',
+                                    1000,
+                                    'I am proficient in Jira',
+                                    1000,
+                                    'I am proficient in Linux',
+                                    1000,
                                 ]}
                                 wrapper="span"
                                 speed={50}
-                                style={{ fontSize: '2em', display: 'inline-block' }}
+                                style={{ fontSize: '1em', display: 'inline-block' }}
                                 repeat={Infinity}
                             />
 
