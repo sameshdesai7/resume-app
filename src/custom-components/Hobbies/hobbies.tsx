1e7f389c850787f6cc06ad58a0758719f64f7ff9
sameshdesai7 <srd00013@mix.wvu.edu>
2024-09-30 09:54:00 -0400

    Reduced Hobbies cards to one row. Consolidated card format with skills row.

--- src/custom-components/Hobbies/hobbies.tsx
+++ src/custom-components/Hobbies/hobbies.tsx
@@ -1,22 +1,75 @@
 import React from 'react';
-import {StlViewer} from "react-stl-viewer";
-import { Stack } from '@mui/material';
+import { StlViewer } from "react-stl-viewer";
+import { Box, Card, CardContent, Stack } from '@mui/material';
+import Grid from '@mui/material/Grid2';
 import { Typography } from '@mui/material';
-import Cubes from '../Hobbies/rubix-cubes';
-import Gaming from '../Hobbies/gaming';
-import Health from '../Hobbies/health';
-
 
 export default function Hobbies() {
+
+    const url = require('../../assets/cube.stl')
+
+    const style = {
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '400px',
+    }
+
     return (
 
-        <Stack spacing = {2}>
-            <Typography variant="h4" sx={{fontFamily: "monospace", paddingBottom: '50px',}}>Hobbies</Typography>
-            <Cubes/>
-            <Gaming/>
-            <Health/>
-        </Stack>
-        
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'left' }}>
+            <Typography variant="h4" sx={{ fontFamily: "monospace", paddingBottom: '50px', }}>Hobbies</Typography>
+            <Grid container spacing={2} sx={{ width: "90%", display: "flex", justifyContent: "center" }}>
+                <Grid size={4} sx={{ display: "flex", justifyContent: "center" }}>
+                    <Card sx={{ backgroundColor: "background.default", width: "100%", maxWidth: "500px", borderRadius: "10px", boxShadow: 3 }}>
+                        <CardContent>
+                            <Typography variant="h5" component="h5" sx={{ fontFamily: "monospace", color: "primary.main" }}>
+                                Rubix Cubes
+                            </Typography>
+                            <StlViewer
+                                style={style}
+                                orbitControls
+                                shadows
+                                url={url}
+                            />
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid size={4} sx={{ display: "flex", justifyContent: "center" }}>
+                    <Card sx={{ backgroundColor: "background.default", width: "100%", maxWidth: "500px", borderRadius: "10px", boxShadow: 3 }}>
+                        <CardContent>
+                            <Typography variant="h5" component="h5" sx={{ fontFamily: "monospace", color: "primary.main" }}>
+                                Counter Strike 2
+                            </Typography>
+                            <Typography variant="body1" component="p" sx={{ fontFamily: "", color: "text.secondary" }}>
+                                <ul>
+                                    <li>Skills Learned: Communication, Teamwork, Quick Thinking, Hand Eye Coordination</li>
+                                    <li>Highest Rank: 17000 Elo</li>
+                                    <li>Hours Played: 3000+</li>
+                                </ul>
+                            </Typography>
+
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid size={4} sx={{ display: "flex", justifyContent: "center" }}>
+                    <Card sx={{ backgroundColor: "background.default", width: "100%", maxWidth: "500px", borderRadius: "10px", boxShadow: 3 }}>
+                        <CardContent>
+                            <Typography variant="h5" component="h5" sx={{ fontFamily: "monospace", color: "primary.main" }}>
+                                Exercise
+                            </Typography>
+                            <Typography variant="body1" component="p" sx={{ fontFamily: "", color: "text.secondary" }}>
+                                <ul>
+                                    <li>Placeholder 1</li>
+                                    <li>Placeholder 2</li>
+                                </ul>
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </Grid>
+        </Box>
+
     );
 }
 
